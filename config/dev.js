4a0cd78ad0faf53c0d4a4349d38f0bb601c55337
ashrafhussain17 <ashrafhussain.2317.csedu@gmail.com>
2020-05-31 19:54:36 +0600

    initial phase is done

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    "28675654046-epf18bsvrcce557vuiadrasi7f5517q5.apps.googleusercontent.com",
+  googleClientSecret: "2VMr9LN3sSIAe2FZuGXLOQvy",
+  mongoURI: "mongodb://localhost:27017/stephan",
+  cookieKey: "123123123",
+};
